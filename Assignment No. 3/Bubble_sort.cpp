929498b26de228cfe7aeb4d564f8e0af5657e3bf
Rushi <kolgatramrushikesh7800@gmail.com>
2023-02-21 13:44:38 +0530

    Assignment 5 Updated

--- Assignment No. 3/Bubble_sort.cpp
+++ Assignment No. 3/Bubble_sort.cpp
@@ -37,7 +37,5 @@ int main(){
         cout<<a[i]<<" ";
       }
       cout<<endl;
-
-
 return 0;
 }
---
+++ Assignment No. 5/Quick_sort.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long 
+#define ld long double
+#define fi first
+#define se second
+#define pb push_back
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define vll vector<ll>
+#define vpi vector<pii>
+#define inf 1e18
+#define mod 1000*1000*1000 + 7
+#define mod1 998244353
+int main(){
+    int n;
+    cout<<"Enter size of an array : ";
+    cin>>n;
+    int arr[n];
+    for( int i = 0; i < n; i++){
+        cin>>arr[i];
+    }
+   
+
+}
+
